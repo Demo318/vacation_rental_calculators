22caf062d1c3e41b5d70bc9754becb263f96431c
Devin Mork <devin.mork@gmail.com>
2022-12-09 15:52:44 -0600

    Begin creating first calculator.

--- vacation_rental_calculators/calculators/models.py
+++ vacation_rental_calculators/calculators/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class DynamicCleaningFeeCalculation(models.Model):
+    cleaner_payout = models.DecimalField(max_digits=6, decimal_places=2)
+    clean_flat_rate = models.DecimalField(max_digits=6, decimal_places=2)
+    clean_dynamic_rate_1 = models.DecimalField(max_digits=6, decimal_places=2)
+    clean_dynamic_rate_2 = models.DecimalField(max_digits=6, decimal_places=2)
+    clean_dynamic_rate_3 = models.DecimalField(max_digits=6, decimal_places=2)
+    clean_dynamic_rate_4 = models.DecimalField(max_digits=6, decimal_places=2)
+
--- vacation_rental_calculators/calculators/views.py
+++ vacation_rental_calculators/calculators/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 def index(request):
-    return HttpResponse("It's alive!")
+    return render(request, 'calculators/index.html')
+
+class DynamicCleansFeesView(TemplateView):
+    template_name = 'index.html'
